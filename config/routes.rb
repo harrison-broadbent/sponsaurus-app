11cc0e247821fcfa9b63cfc2a2313e39161355e2
Harrison Broadbent <harrisonbroadbent@gmail.com>
2021-05-31 19:57:35 +1000

    Scaffold User, Fix Devise routes, Minor styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  # devise_for must sit above resources :users
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [:show]
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
