597e0f12832cb3eb0a59b3c46ede0229aa02a100
Harrison Broadbent <harrisonbroadbent@gmail.com>
2021-08-10 14:48:45 +1000

    add length validations and adjust forms

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -23,7 +23,7 @@ class Newsletter < ApplicationRecord
 
   # Pretty URL Slugs
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :name, use: %i[slugged]
 
   # Associations
   belongs_to :user
@@ -31,29 +31,8 @@ class Newsletter < ApplicationRecord
   has_many :slot_types, dependent: :destroy
 
   # Validations
+  validates :name, presence: true, length: { in: 2..40 }
+  validates :information, presence: true, length: { in: 2..300 }
+  validates :statistics, presence: true, length: { in: 2..100 }
 
-  validates :name, presence: true
-  validates :information, presence: true
-  validates :statistics, presence: true
-
-  # Methods
-  #   def set_default_email_content
-  #     self.email_template_subject = "#{self.name} sponsorship enquiry"
-  #     self.email_template_body = "Hey, I'm getting in touch to book a sponsorship slot in your newsletter.
-  # Here are the relevant details:
-  # Ad Details -
-  # Name: [Insert Product Name]
-  # Tagline: [Insert Product Tagline]
-  # Body: [Insert Body Text]
-  # Link: [Insert link to Product]
-  # Image: [Add an image (optional)]
-  #
-  # My Details -
-  # Name: [Name of contact reference]
-  # Email: [Email of contact reference]
-  # Role: [Your role and relationship to the product]
-  # [NOTE TO SENDER: Please fill in ALL the following to ensure your inquiry is taken seriously.]
-  # [Feel free to delete the text in [brackets] before sending]
-  # "
-  #   end
 end
