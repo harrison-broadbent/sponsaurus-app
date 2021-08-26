7736cd109d9ca3beeb693310766b9bd4b8aeef5d
Harrison Broadbent <harrisonbroadbent@gmail.com>
2021-08-26 15:55:12 +1000

    allow embedding of individual slot types

--- app/javascript/packs/slots.js
+++ app/javascript/packs/slots.js
@@ -22,34 +22,8 @@ function copyShareURL() {
     btn.innerText = "Link copied to clipboard!"
 }
 
-// Copies code to embed the current url into another page.
-
-function copyEmbedCode() {
-    // creates a temporary element and copies from it.
-
-    let temp_element = document.createElement('input'),
-        text = window.location.href +  "/embed";
-
-    let embed_code = `<iframe style="width: 80%; height: 400px; margin: 0 auto; border: none" src="${text}"></iframe>`
-
-    document.body.appendChild(temp_element);
-    temp_element.value = embed_code;
-    temp_element.select();
-    document.execCommand('copy');
-    document.body.removeChild(temp_element);
-
-    console.log("copied link!")
-
-    let btn = document.getElementById("button__embed-link")
-    btn.innerText = "Code copied to clipboard!"
-}
-
 if (document.getElementById("button__share-link")) {
     document.getElementById("button__share-link").addEventListener('click', copyShareURL)
     console.log("copied url")
 }
 
-if (document.getElementById("button__embed-link")) {
-    document.getElementById("button__embed-link").addEventListener('click', copyEmbedCode)
-    console.log("copied embed code")
-}
